5f72c5313050f24baebb8ad6dc93859208652748
Lenmor Dimanalata <lenmor@invoicesimple.com>
2025-07-28 17:54:11 -0400

    feat(packages/is-add): add feature X

--- packages/is-even/lib/is-even.ts
+++ packages/is-even/lib/is-even.ts
@@ -1,6 +1,6 @@
 import { isOdd } from "@lenmor-invoicesimple/is-odd-2";
 
 export function isEven(num: number): boolean {
-  console.log("isEven called with 111:", num);
+  console.log("isEven called with 11111:", num);
   return !isOdd(num);
 }
